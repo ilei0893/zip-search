e1e22f213ac00aaf9a5666a5054d1dd599c03a81
Ivan Lei <ilei0893@gmail.com>
2020-06-04 13:30:15 -0400

    Added enter zip input box

--- src/Components/Zipsearch.js
+++ src/Components/Zipsearch.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class Zipsearch extends Component{
 constructor(props){  
   super(props);
-  this.state = {zip: ""};
+  this.state = {
+      zip: "10016"
+    };
 }
 
+    handleChange(event)
+    {
+        this.setState = ({zip: event.target.zip});
+    }
     render() {
-        return <h1>Hello</h1>;
+        return (
+            <div className = "search">
+                <label for="zip">Enter a zip code: </label>
+                <input type="text" value={this.state.zip} onChange = {this.handleChange}></input>
+            </div>
+        );
     }
 }
 
