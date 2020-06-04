d2e045f421f1fe7ea2e71fdd3e0643f2235d3ac4
Rehan Khan <rehangh8924@gmail.com>
2020-06-04 13:29:32 -0400

    added the simple layout of the page from the example before search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import Zipsearch from "./components/Zipsearch";
+import Zipsearch from "./Components/Zipsearch";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <h1 className="main-header">Zip Code</h1>
+      <p>Zip Code: </p>
+      <input type="number" placeholder="Try 10016"></input>
+      <p>No Results</p>
+      <Zipsearch />
     </div>
   );
 }
--- src/Components/Zipsearch.js
+++ src/Components/Zipsearch.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class Zipsearch extends Component{
-    
+    state ={ zip: null};
+
+    componentDidMount(){
+        axios
+        .get("http://ctp-zip-api.herokuapp.com/zip/" + this.props.name)
+        .then((respone) => {
+            const data = respone.data;
+
+            const newZipObj={
+                city: data.City,
+            }
+        });
+    }
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
 }
 export default Zipsearch;
