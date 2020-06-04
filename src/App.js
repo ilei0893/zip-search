9240572c3cddceb0415b534076878f04093eff8e
Rehan Khan <rehangh8924@gmail.com>
2020-06-04 14:04:00 -0400

    fixed css issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import "./App.css"
 import Zipsearch from "./Components/Zipsearch";
 
 class App extends Component{
   render() {
     return (
       <div className="Zip">
-        <h1>Zip Searcher</h1>
+        <h1 className="main-header">Zip Searcher</h1>
         <Zipsearch zip=""/>
       </div>
     );
