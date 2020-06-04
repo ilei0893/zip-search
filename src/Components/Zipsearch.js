0f2e4d7a505f635b8dfb0ee7a5580d6a39a2292e
Rehan Khan <rehangh8924@gmail.com>
2020-06-04 14:27:13 -0400

    center the layout of the page

--- src/Components/Zipsearch.js
+++ src/Components/Zipsearch.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import "../App.css"
 
 class Zipsearch extends Component{
 constructor(props){  
   super(props);
   this.state = {
-      zip: "10016"
+      zip: null
     };
+    this.handleZipChange = this.handleZipChange.bind(this);
 }
 
-    handleChange(event)
-    {
-        this.setState = ({zip: event.target.zip});
-    }
+    handleZipChange(event)
+    { this.setState = ({zip: event.target.zip}); }
+
+
+
     render() {
         return (
             <div className = "search">
                 <label for="zip">Enter a zip code: </label>
-                <input type="text" value={this.state.zip} onChange = {this.handleChange}></input>
+                <input placeholder="Try 10016" type="text" value={this.state.zip} onChange = {this.handleZipChange}></input>
             </div>
         );
     }
